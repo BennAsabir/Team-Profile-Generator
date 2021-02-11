033f381d6fa7ceec2661e2036ab524c8f2b29f7e
Benzino <benasabir@gmail.com>
2021-02-11 10:52:10 -0500

    update all tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -7,14 +7,15 @@ test("Can set school via constructor", () => {
   expect(e.school).toBe(testValue);
 });
 
+test("Can get school via getSchool()", () => {
+    const testValue = "UofT";
+    const e = new Intern("Ren", 1, "test@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+  });
+
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
   const e = new Intern("Ren", 1, "test@test.com", "UofT");
   expect(e.getRole()).toBe(testValue);
 });
 
-test("Can get school via getSchool()", () => {
-  const testValue = "UofT";
-  const e = new Intern("Ren", 1, "test@test.com", testValue);
-  expect(e.getSchool()).toBe(testValue);
-});
